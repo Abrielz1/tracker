caf516ed7f874c86e301f09b2bc05525aefaa569
Abriel <abrielz11@gmail.com>
2024-01-20 19:07:38 +0300

    task service update was completed

--- src/main/java/com/example/tracker/service/TaskService.java
+++ src/main/java/com/example/tracker/service/TaskService.java
@@ -1,12 +1,18 @@
 package com.example.tracker.service;
 
 import com.example.tracker.dto.TaskDto;
+import com.example.tracker.model.Task;
+import com.example.tracker.model.User;
 import com.example.tracker.repository.TaskRepository;
+import com.example.tracker.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.time.Instant;
 import java.time.LocalDateTime;
 import static com.example.tracker.mapper.TaskMapper.TASK_MAPPER;
 
@@ -16,6 +22,8 @@ import static com.example.tracker.mapper.TaskMapper.TASK_MAPPER;
 public class TaskService {
 
     private final TaskRepository repository;
+
+    private final UserRepository userRepository;
 
     // TODO: найти все задачи
     //  (в ответе также должны находиться вложенные сущности, которые описывают автора задачи и исполнителя,
@@ -48,7 +56,54 @@ public class TaskService {
 
         log.info("Task with id: {} and with userId: {}" +
                 " was updated via service at" + " time: " + LocalDateTime.now(), id, userId);
-        return null; //todo: дописать логику обновления
+
+
+        Task task = repository.findById(id).block();
+        User user = userRepository.findById(userId).block();
+
+        if (StringUtils.hasText(taskDto.getName())) {
+            if (task != null) { // TODO: проверить этот совет idea
+                task.setName(taskDto.getName());
+            }
+        }
+
+        if (StringUtils.hasText(taskDto.getDescription())) {
+            if (task != null) {
+                task.setDescription(taskDto.getDescription());
+            }
+        }
+
+        if (taskDto.getCreatedAt() != null && taskDto.getCreatedAt().isBefore(Instant.now())) {
+            if (task != null) {
+            task.setCreatedAt(taskDto.getCreatedAt());
+            }
+        }
+
+        if (taskDto.getUpdatedAt() != null && taskDto.getUpdatedAt().isBefore(Instant.now())) {
+            if (task != null) {
+                task.setUpdatedAt(taskDto.getUpdatedAt());
+            }
+        }
+
+        if (StringUtils.hasText(taskDto.getAssigneeId())) { // TODO: проверить тз
+            if (task != null) {
+                task.setAssigneeId(taskDto.getAssigneeId());
+            }
+        }
+
+        if (StringUtils.hasText(taskDto.getAuthorId())) { // TODO: проверить тз
+            if (task != null) {
+                task.setAuthorId(taskDto.getAuthorId());
+            }
+        }
+
+        if (taskDto.getStatus() != null) {
+            if (task != null) {
+                task.setStatus(taskDto.getStatus());
+            }
+        }
+
+        return repository.save(TASK_MAPPER.toTask(taskDto)).map(TASK_MAPPER::toTaskDto);
     }
 
     public Mono<Void> removeById(String id) {
